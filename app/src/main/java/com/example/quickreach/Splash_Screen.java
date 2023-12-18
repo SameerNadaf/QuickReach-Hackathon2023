50064f4ff0c6ce720d584245851966773bc4b782
Sameer Nadaf <nsameernadaf@gmail.com>
2023-12-18 18:56:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quickreach/Splash_Screen.java
@@ -0,0 +1,30 @@
+package com.example.quickreach;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    private static final long SPLASH_DURATION = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(Splash_Screen.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+
+            }
+
+        }, SPLASH_DURATION);
+    }
+
+}
